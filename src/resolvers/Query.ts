3e0dc379b08fd1b7a3ffefa06e78c21c167c83cf
dherv <damien.herv@gmail.com>
2021-06-27 16:16:16 +0900

    feat: add orderBy to songs query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,7 +1,6 @@
 import { IUser } from '../types/interfaces';
 
-export const songs = async (parent: any, args: any, context: any) =>
-  context.prisma.song.findMany();
+export const songs = async (parent: any, args: any, context: any) => context.prisma.song.findMany({orderBy: args?.orderBy || 'desc'})
 
 export const user = async (parent: any, args: any, context: any) =>
   context.prisma.user.findUnique({ where: { id: context.userId } });
